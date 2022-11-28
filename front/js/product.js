5cdbbb44f27321b803838cae6c0794ae2adfb3e3
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2022-11-29 00:37:23 +0100

    bataille avec le select

--- front/js/product.js
+++ front/js/product.js
@@ -20,10 +20,12 @@ const url = window.location.search;
 const params = new URLSearchParams(url);
 const productId = params.get('id');
 
-async function getProduct () {
-    return fetch(`http://localhost:3000/api/products/${product}`)
+async function getProduct(id) {
+    console.log(id);
+    return fetch(`http://localhost:3000/api/products/${id}`)
     .then(function(result) {
     if (result.ok) {
+        console.log(result);
       return result.json();
     }
   })
@@ -32,3 +34,25 @@ async function getProduct () {
   });
 }
 
+function displayProductSpecs(product){
+    document.getElementsByClassName("item__img").innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`;
+    console.log(product.imageUrl);
+    document.getElementById("title").innerHTML = product.name;
+    document.getElementById("price").innerHTML = product.price;
+    document.getElementById("description").innerHTML = product.description;
+    for (let color of product.colors){
+        let option = document.createElement('option');
+        option.text = `${color}`;
+        option.value = `${color}`;
+        let select = document.getElementsByTagName("select");
+        select.appendChild(option);
+        
+        
+        //document.getElementsByTagName("select").innerHTML += `<option value="${color}">${color}</option>`;
+        let test = document.getElementsByTagName("select");
+        console.log(test);
+    }
+}
+
+let product = await getProduct(productId);//ATTENTION à bien renseigner un type="module" dans le script html
+displayProductSpecs(product);
