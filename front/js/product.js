e7a42efff482147bbea14a2fab21db5c05f298f7
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2022-12-06 22:02:29 +0100

    clean du code et application conseils mentorat

--- front/js/product.js
+++ front/js/product.js
@@ -20,6 +20,7 @@ async function getProduct(id) {
 function displayProductSpecs(product){
     //fetch le node par une autre méthode car classname ne renvoie pas l'élément directement.
     //document.querySelector renvoie bien le node
+    //picDiv = élément dans lequel je vais insérer le reste
     const picDiv = document.querySelector(".item__img"); //Attention il faut conserver le '.' de la classe avec querySelector
     const pic = document.createElement('img');
     pic.setAttribute('src', `${product.imageUrl}`);
@@ -47,68 +48,37 @@ displayProductSpecs(product);
 //récupération de l'input de l'utilisateur
   //definitions de variables et fonction eventHandler
 let quantity = document.getElementById('quantity');
-let quantitySelected;
-let colorOption = document.getElementById('colors');
-let colorSelected;
+let color = document.getElementById('colors');
 
-function quantitySetup(){quantitySelected = quantity.value;}
-function colorSetup(){colorSelected = colorOption.value;}
-
-  //ajout des listeners
-quantity.addEventListener('change', quantitySetup);
-colorOption.addEventListener('change', colorSetup);
-
-//creation du panier en localStorage
 //ajout de l'eventHandler sur le bouton 'ajouter au panier'
 const submit = document.getElementById('addToCart');
 submit.addEventListener('click', updateCart);
 
-let i = 1; //pour incrémenter un indice de stockage dans localStorage
+//creation de la sélection
+const selection = {
+id: productId,
+qty: quantity.value,
+color: color.value
+};
 
-function updateCart(){
-  //creation de l'objet souhaité
-  const selection = {
-  id: productId,
-  qty: quantitySelected,
-  color: colorSelected
-  };
-
-  let doublon = 0; //pour checker si un article est déjà présent dans le localStorage
-  //valeur reset à zéro à chaque appel de updateCart
-  
-  //check si doublon avant insertion panier
-  //récupération du local storage dans un array
-  let currentStorage = [];
-  for (let i = 0; i<localStorage.length; i++){
-    let clef = localStorage.key(i);
-    currentStorage += localStorage.getItem(clef);
-    console.log(currentStorage);
+//récupération du panier
+function getBasket(){
+    let basket = localStorage.getItem('basket');
+    if (basket == null){
+    return [];
+  }else{
+    return JSON.parse(basket);
   }
-  //currentStorage.shift(); //removes null value at the beginning of the array due to variable init on blank
-
-  //comparaison de la selection contre l'array récupéré
-  if (currentStorage == !null){
-    for (let item of currentStorage){
-      let itemJson = JSON.parse(item);
-      if (itemJson.id == selection.id && itemJson.color == selection.color){
-        doublon ++;
-      }   
-    }
-  }
-
-  //si doublon est false -> not 1 (la valeur ne peut être que de 0 ou 1), il n'y a pas de doublon donc on push dans el local storage
-  if (!doublon){
-      let article = JSON.stringify(selection);
-      localStorage.setItem(i, article);
-      console.log(localStorage.getItem(i));
-      i++;
-    }else{
-      alert(`L'article choisi est déjà présent dans votre panier, vous venez d'ajouter ${doublon} exemplaire au panier`);
-      //ici il faut incrémenter le nombre d'article dans le localStorage
-    }   
 }
+
+//check doublon et ajout de l'article dans le hash js
+function checkProduct(product){}
+
+//push de l'article dans le localStorage
+function updateCart(){}
+
+
+
 
 //méthodes pour clear localStorage et vérifier qu'il est vide. Attention, il reste indice 1 car il est set à 1 avant l'appel de updateCart
 //setTimeout(() => {window.localStorage.clear();}, 5000);
-
-
